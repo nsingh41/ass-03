0e5af0ccea17c253609887cf6bead8c32307339e
Nav Jot <kanda.navjotsingh@gmail.com>
2018-03-21 10:12:29 -0700

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.upload = (event) => {
+  event.Records.forEach((record) => {
+    const filename = record.s3.object.key;
+    const filesize = record.s3.object.size;
+    const eventTime = record.eventTime
+
+    const params = {
+      TableName: 'special',
+      Item: {
+        id: uuid.v1(),
+        Name: filename,
+        size: filesize,
+        eventTime: eventTime
+      }
+    }
+
+    dynamoDb.put(params, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+    })
+
+  });
+};
